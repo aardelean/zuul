71df696e3dece9531d374578071c173003819508
aardelean <alex.ardelean@appdirect.com>
2016-08-30 14:16:17 +0200

    fixed loggings with logstash and oauth2 sso

--- src/main/groovy/home/consul/zuul/ZuulApplication.groovy
+++ src/main/groovy/home/consul/zuul/ZuulApplication.groovy
@@ -1,6 +1,8 @@
 package home.consul.zuul
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 import org.springframework.cloud.netflix.hystrix.EnableHystrix
@@ -19,15 +21,18 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableHystrix
 @EnableHystrixDashboard
 @EnableDiscoveryClient
-//@EnableOAuth2Sso
+@EnableOAuth2Sso
 @Import([EndpointConfiguration])
 class ZuulApplication extends WebSecurityConfigurerAdapter {
+
+    @Value('#{\'${unprotectedResources}\'.split(\',\')}')
+    List<String> unprotectedResources
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/**", "/refresh").permitAll()
+                .antMatchers(unprotectedResources.toArray(new String[unprotectedResources.size()])).permitAll()
                 .anyRequest().authenticated()
     }
 
