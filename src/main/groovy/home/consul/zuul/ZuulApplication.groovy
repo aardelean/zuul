4f0f8e85b401cb346746fd81a3b7fe5d24fd01ec
aardelean <alex.ardelean@appdirect.com>
2017-02-14 17:52:57 +0100

    adapted zuul for appdirect oauth2SSO usage

--- src/main/groovy/home/consul/zuul/ZuulApplication.groovy
+++ src/main/groovy/home/consul/zuul/ZuulApplication.groovy
@@ -5,35 +5,39 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.hystrix.EnableHystrix
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
-import org.springframework.cloud.netflix.turbine.EnableTurbine
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Import
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 
 @Configuration
 @EnableAutoConfiguration
 @EnableZuulProxy
-@EnableTurbine
-@EnableHystrix
-@EnableHystrixDashboard
 @EnableDiscoveryClient
 @EnableOAuth2Sso
-@Import([EndpointConfiguration])
 class ZuulApplication extends WebSecurityConfigurerAdapter {
 
     @Value('#{\'${unprotectedResources}\'.split(\',\')}')
     List<String> unprotectedResources
+    @Value('#{\'${userResources}\'.split(\',\')}')
+    List<String> userResources
+    @Value('#{\'${channelAdminResources}\'.split(\',\')}')
+    List<String> channelAdminResources
+    @Value('#{\'${superuserResources}\'.split(\',\')}')
+    List<String> superuserResources
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers(unprotectedResources.toArray(new String[unprotectedResources.size()])).permitAll()
-                .anyRequest().authenticated()
+                .antMatchers(splitPaths(userResources)).hasRole("USER")
+                .antMatchers(splitPaths(channelAdminResources)).hasRole("CHANNEL_ADMIN")
+                .antMatchers(splitPaths(superuserResources)).hasRole("SUPERUSER")
+                .antMatchers(splitPaths(unprotectedResources)).permitAll()
+    }
+
+    private String[] splitPaths(List<String> resources) {
+        resources.toArray(new String[resources.size()])
     }
 
     static void main(String[] args) {
